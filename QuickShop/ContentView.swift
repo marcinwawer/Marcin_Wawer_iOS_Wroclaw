dd9a3afe0db9be0fe357a1d1d4d5c8384785609d
marcin wawer <m.wawer.git@icloud.com>
2025-05-10 12:29:16 +0200

    chore: project setup

---
+++ QuickShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickShop
+//
+//  Created by Marcin Wawer on 10-05-2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickShop/QuickShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickShopApp.swift
+//  QuickShop
+//
+//  Created by Marcin Wawer on 10-05-2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
